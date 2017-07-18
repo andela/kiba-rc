901b294665ed48810279d007476b985f32632a5e
Johnbosco Ohia <johnbosco.ohia@andela.com>
2017-07-18 10:29:32 +0100

    feature(wallet): fix fund wallet button  [Finishes #147504927]

--- server/publications/collections/wallet.js
+++ server/publications/collections/wallet.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-import { Wallet } from "/lib/collections";
-
-Meteor.publish("transactions", (userId) => {
-  check(userId, String);
-  return Wallet.find({ userId });
-=======
 import { Wallets } from "/lib/collections";
 
 Meteor.publish("transactionDetails", (userId) => {
   check(userId, String);
   return Wallets.find({ userId });
->>>>>>> b539fecfacd1da9679d2586e4eb02ee1f07b94fb
 });
