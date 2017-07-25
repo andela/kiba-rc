38eadafe843b85e27fb1607724de9989259021b0
Andela Developer <mayowa.makinde@andela.com>
2017-07-25 12:46:59 +0100

    add vendors profile and signup form

--- client/modules/accounts/templates/signUp/signUp.js
+++ client/modules/accounts/templates/signUp/signUp.js
@@ -16,7 +16,9 @@ Template.loginFormSignUpView.onCreated(() => {
  * Helpers: Login form sign up view
  */
 Template.loginFormSignUpView.helpers(LoginFormSharedHelpers);
-
+Template.loginFormSignUpView.onRendered(function () {
+  $(".vendor-form").css("display", "none");
+});
 /**
  * Events: Login form sign up view
  */
@@ -30,13 +32,15 @@ Template.loginFormSignUpView.events({
   "submit form": function (event, template) {
     event.preventDefault();
 
-    // var usernameInput = template.$(".login-input--username");
+    const usernameInput = template.$(".login-input--username");
     const emailInput = template.$(".login-input-email");
     const passwordInput = template.$(".login-input-password");
 
+    const username = usernameInput.val().trim();
     const email = emailInput.val().trim();
     const password = passwordInput.val().trim();
 
+    const validatedUsername = LoginFormValidation.username(username);
     const validatedEmail = LoginFormValidation.email(email);
     const validatedPassword = LoginFormValidation.password(password);
 
@@ -45,6 +49,9 @@ Template.loginFormSignUpView.events({
 
     templateInstance.formMessages.set({});
 
+    if (validatedUsername !== true) {
+      errors.username = validatedUsername;
+    }
     if (validatedEmail !== true) {
       errors.email = validatedEmail;
     }
@@ -52,6 +59,41 @@ Template.loginFormSignUpView.events({
     if (validatedPassword !== true) {
       errors.password = validatedPassword;
     }
+
+    let vendorDetails = {};
+
+    if (Session.get("signupas") === "Vendor") {
+      shopName = template.$(".shop-name").val().trim();
+      shopPhone = template.$(".shop-phone").val().trim();
+      shopAddress = template.$(".shop-address").val().trim();
+
+      const validatedShopName = LoginFormValidation.shopName(shopName);
+      const validatedShopPhone = LoginFormValidation.shopPhone(shopPhone);
+      const validatedShopAddress = LoginFormValidation.shopAddress(shopAddress);
+
+      if (validatedShopName !== true) {
+        errors.shopName = validatedShopName;
+      }
+
+      if (validatedShopPhone !== true) {
+        errors.shopPhone = validatedShopPhone;
+      }
+
+      if (validatedShopAddress !== true) {
+        errors.shopAddress = validatedShopAddress;
+      }
+
+      vendorDetails = {
+        vendorDetails: [{
+          shopName: shopName,
+          shopPhone: shopPhone,
+          shopAddress: shopAddress,
+          isVendor: true,
+          shopActive: false
+        }]
+      };
+    }
+
 
     if ($.isEmptyObject(errors) === false) {
       templateInstance.formMessages.set({
@@ -62,9 +104,10 @@ Template.loginFormSignUpView.events({
     }
 
     const newUserData = {
-      // username: username,
+      username: username,
       email: email,
-      password: password
+      password: password,
+      profile: vendorDetails
     };
 
     Accounts.createUser(newUserData, function (error) {
@@ -77,5 +120,16 @@ Template.loginFormSignUpView.events({
         // Close dropdown or navigate to page
       }
     });
+  },
+  "change .chooseSignupType": function (event, template) {
+    const element = template.find("input:radio[name=role]:checked");
+    const value = $(element).val();
+    if (value === "asVendor") {
+      $(".vendor-form").css("display", "block");
+      Session.set("signupas", "Vendor");
+    } else {
+      $(".vendor-form").css("display", "none");
+      Session.set("signupas", "Customer");
+    }
   }
 });
